57ce31aaa59cad19c684eee2853c0c12648f8d02
Gregory Neal <gnealsr@gmail.com>
2017-12-29 11:35:24 -0600

    Adding Conversation processing

--- migrations/0001.sql
+++ migrations/0001.sql
@@ -6,10 +6,17 @@ CREATE TABLE users (
 	active BOOLEAN DEFAULT true
 );
 
+CREATE TABLE conversations (
+	id SERIAL NOT NULL PRIMARY KEY,
+	conversation_key VARCHAR,
+	from_userid INTEGER,
+	to_userid INTEGER
+);
+
 CREATE TABLE messages (
 	id SERIAL NOT NULL PRIMARY KEY,
-	message_key VARCHAR,
+	conversation_key VARCHAR,
 	message TEXT,
 	datetime_sent TIMESTAMP,
-	users_id INTEGER REFERENCES users (id)
+	sent_by INTEGER REFERENCES users (id)
 );
--- routes.js
+++ routes.js
@@ -2,7 +2,10 @@
 const pgp = require('pg-promise')({});
 const db = pgp(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/messaging');
 
-// Tool for assigning a unique id
+// Tool for formatting urls
+const url = require('url');
+
+// Tool for assigning a unique conversation id
 const uuidv4 = require('uuid/v4');
 
 // Tool for retrieving current date/time
@@ -12,23 +15,59 @@ module.exports = function(app) {
 
   // get method for root URL:/
   app.get('/', function (req, resp, next) {
+    var context = {
+      title: 'Messaging App'
+    };
+    resp.render('index.hbs', context);
+  });
+
+  // post method for root URL:/
+  app.post('/', function (req, resp, next) {
+    var from_id = req.body.from_id;
+    var to_id = req.body.to_id;
+    // get conversation key
+    var q = 'SELECT * FROM conversations \
+    WHERE from_userid = $1 OR to_userid = $1 \
+    AND from_userid = $2 OR to_userid = $2';
+    db.any(q, from_id, to_id)
+
+    // assign a unique conversation key
     var key = uuidv4();
-    resp.redirect('/message/' + key);
+    var table_data = {
+      conversation_key: key,
+      from_id: req.body.from_id,
+      to_id: req.body.to_id
+    };
+    var q = 'INSERT INTO conversations \
+    VALUES (default, ${conversation_key}, ${from_id}, ${to_id})';
+    db.any(q, table_data)
+      .then(function () {
+        resp.redirect(url.format({
+          pathname: "/message",
+          query: {
+            "key": key,
+            "from": req.body.from_id
+          }
+        }));
+      })
+      .catch(next);
   });
 
   // get method for messaging
-  app.get('/message/:key', function (req, resp, next) {
-    var key = req.params.key;
+  app.get('/message', function (req, resp, next) {
+    var key = req.query.key;
+    var from_id = req.query.from;
     var q = 'SELECT * FROM messages \
-    LEFT JOIN users ON users.id = messages.users_id \
-    WHERE message_key = $1 \
+    LEFT JOIN users ON users.id = messages.sent_by \
+    WHERE conversation_key = $1 \
     ORDER BY datetime_sent DESC';
     db.any(q, key)
       .then(function (results) {
         var context = {
           title: 'Messaging',
           key: key,
-          messages: results
+          from_id: from_id,
+          message: results
         };
         resp.render('message.hbs', context);
       })
@@ -40,15 +79,22 @@ module.exports = function(app) {
     var key = req.body.key;
     var now = moment();
     var table_data = {
-      message_key: key,
+      conversation_key: key,
       message: req.body.message,
-      datetime_sent: now
+      datetime_sent: now,
+      sent_by: req.body.from_id
     };
     var q = 'INSERT INTO messages \
-    VALUES (default, ${message_key}, ${message}, ${datetime_sent})';
+    VALUES (default, ${conversation_key}, ${message}, ${datetime_sent}, ${sent_by})';
     db.any(q, table_data)
-      .then(function (results) {
-        resp.redirect('/message/' + key);
+      .then(function () {
+        resp.redirect(url.format({
+          pathname: "/message",
+          query: {
+            "key": key,
+            "from": req.body.from_id
+          }
+        }));
       })
       .catch(next);
   });
