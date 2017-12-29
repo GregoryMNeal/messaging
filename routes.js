84ebb7a4f5f86669eda3c9f071f96d88fc8fdb1d
Gregory Neal <gnealsr@gmail.com>
2017-12-29 14:23:58 -0600

    Refactor with prompt URL

--- routes.js
+++ routes.js
@@ -9,26 +9,26 @@ const url = require('url');
 const uuidv4 = require('uuid/v4');
 
 // Tool for retrieving current date/time
-var moment = require('moment');
+const moment = require('moment');
 
 module.exports = function(app) {
 
   // get method for root URL:/
-  app.get('/', function (req, resp, next) {
-    var context = {
+  app.get('/prompt', function (req, resp, next) {
+    let context = {
       title: 'Messaging App'
     };
-    resp.render('index.hbs', context);
+    resp.render('prompt.hbs', context);
   });
 
   // post method for root URL:/
-  app.post('/', function (req, resp, next) {
+  app.post('/prompt', function (req, resp, next) {
     // get conversation key
-    var select_data = {
+    let select_data = {
       from_id: req.body.from_id,
       to_id: req.body.to_id
     };
-    var q = 'SELECT * FROM conversations \
+    let q = 'SELECT * FROM conversations \
     WHERE from_userid = ${from_id} OR to_userid = ${from_id} \
     AND from_userid = ${to_id} OR to_userid = ${to_id}';
     db.any(q, select_data)
@@ -45,13 +45,13 @@ module.exports = function(app) {
           }));
         } else {
           // assign unique conversation key
-          var key = uuidv4();
-          var table_data = {
+          let key = uuidv4();
+          let table_data = {
             conversation_key: key,
             from_id: req.body.from_id,
             to_id: req.body.to_id
           };
-          var q = 'INSERT INTO conversations \
+          let q = 'INSERT INTO conversations \
           VALUES (default, ${conversation_key}, ${from_id}, ${to_id})';
           db.any(q, table_data)
             .then(function (r) {
@@ -74,15 +74,15 @@ module.exports = function(app) {
 
   // get method for messaging
   app.get('/message', function (req, resp, next) {
-    var key = req.query.key;
-    var from_id = req.query.from;
-    var q = 'SELECT * FROM messages \
+    let key = req.query.key;
+    let from_id = req.query.from;
+    let q = 'SELECT * FROM messages \
     LEFT JOIN users ON users.id = messages.sent_by \
     WHERE conversation_key = $1 \
     ORDER BY datetime_sent DESC';
     db.any(q, key)
       .then(function (results) {
-        var context = {
+        let context = {
           title: 'Messaging',
           key: key,
           from_id: from_id,
@@ -95,15 +95,15 @@ module.exports = function(app) {
 
   // post method for messaging
   app.post('/message', function (req, resp, next) {
-    var key = req.body.key;
-    var now = moment();
-    var table_data = {
+    let key = req.body.key;
+    let now = moment();
+    let table_data = {
       conversation_key: key,
       message: req.body.message,
       datetime_sent: now,
       sent_by: req.body.from_id
     };
-    var q = 'INSERT INTO messages \
+    let q = 'INSERT INTO messages \
     VALUES (default, ${conversation_key}, ${message}, ${datetime_sent}, ${sent_by})';
     db.any(q, table_data)
       .then(function () {
